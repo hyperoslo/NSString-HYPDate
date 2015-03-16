f6aa744c70bdd3f8cb4228bc57add841d3ae1ea8
Elvis Nuñez <elvisnunez@me.com>
2015-03-16 11:41:01 +0100

    Improve tests

--- Tests/Tests/Tests.m
+++ Tests/Tests/Tests.m
@@ -2,6 +2,8 @@
 
 #import "NSString+HYPDate.h"
 
+#import "NSDate+HYPConvenience.h"
+
 @interface Tests : XCTestCase
 
 @end
@@ -31,33 +33,41 @@
 
 - (void)testTimeStringInstanceMethod
 {
-
+    XCTAssertEqualObjects(@"09:35", [@"2015-03-12T08:35:56.804Z" hyp_timeString]);
 }
 
 - (void)testDateStringInstanceMethod
 {
-
+    XCTAssertEqualObjects(@"2000-01-01", [@"2000-01-01T10:10:00.002Z" hyp_dateString]);
 }
 
 - (void)testDateStringWithFormatInstanceMethod
 {
-
+    XCTAssertEqualObjects(@"01.01.2000", [@"2000-01-01T10:10:00.002Z" hyp_dateStringWithFormat:@"dd.MM.yyyy"]);
 }
-
 
 - (void)testTimeRangeStringToEndDateString
 {
+    NSString *startDateString = @"2000-01-01T10:10:00.002Z";
+    NSString *endDateString = @"2000-01-02T10:10:00.002Z";
 
+    XCTAssertEqualObjects(@"11:10 - 11:10", [startDateString hyp_timeRangeStringToEndDateString:endDateString]);
 }
 
 - (void)testDateRangeStringToEndDateString
 {
+    NSString *startDateString = @"2000-01-01T10:10:00.002Z";
+    NSString *endDateString = @"2000-01-02T10:10:00.002Z";
 
+    XCTAssertEqualObjects(@"2000-01-01 - 2000-01-02", [startDateString hyp_dateRangeStringToEndDateString:endDateString]);
 }
 
 - (void)testDateRangeStringToEndDateStringWithFormat
 {
+    NSString *startDateString = @"2000-01-01T10:10:00.002Z";
+    NSString *endDateString = @"2000-01-02T10:10:00.002Z";
 
+    XCTAssertEqualObjects(@"01.01.2000 - 02.01.2000", [startDateString hyp_dateRangeStringToEndDateString:endDateString withFormat:@"dd.MM.yyyy"]);
 }
 
 @end
